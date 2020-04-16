1301ad6d56aee75a426b1fce8e527537d94edab0
lili <xiaozhiliaoo@gmail.com>
2020-04-16 21:49:23 +0800

    add forkjoin

---
+++ Douglas-Schmidt-CS891/src/main/java/org/lili/ForkJoinPoolTest.java
@@ -0,0 +1,20 @@
+package org.lili;
+
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * @author lili
+ * @date 2020/4/15 23:24
+ * @description
+ * @notes
+ */
+public class ForkJoinPoolTest {
+    public static void main(String[] args) {
+        System.out.println(ForkJoinPool.getCommonPoolParallelism());
+
+        int numberOfThreads = 10;
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", String.valueOf(numberOfThreads));
+        System.out.println(ForkJoinPool.getCommonPoolParallelism());
+
+    }
+}
